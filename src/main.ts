0bc842dff445deca1c3cf95d51c17e8f7f93705c
Túlio Fergulha <Tulio.Fergulha@AB-Inbev.com>
2023-03-14 21:41:08 -0300

    Adjust test execution

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ const address = new Address("Rua dois", 2, "12345-678", "São Paulo")
 customer.Address = address;
 customer.activate();
 
-const item1 = new OrderItem("1", "Item 1", 10);
-const item2 = new OrderItem("2", "Item 2", 15);
+const item1 = new OrderItem("1", "1", "Item 1", 10, 2);
+const item2 = new OrderItem("2", "2", "Item 2", 15, 3);
 const order = new Order("1", "1", [item1, item2]);
