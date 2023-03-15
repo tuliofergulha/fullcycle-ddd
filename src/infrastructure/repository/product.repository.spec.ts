78b30bb5d72a5e43be3bc3c3a5a1e067356552e1
Túlio Fergulha <Tulio.Fergulha@AB-Inbev.com>
2023-03-14 23:33:34 -0300

    Add sequelize implementation

--- src/infrastructure/repository/product.repository.spec.ts
+++ src/infrastructure/repository/product.repository.spec.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize-typescript";
+import ProductModel from "../db/sequelize/model/product.model";
 
 describe("Product repository test", () => {
-
     let sequilize: Sequelize;
 
     beforeEach(async () => {
@@ -10,12 +10,13 @@ describe("Product repository test", () => {
             storage: 'memory',
             logging: false,
             sync: { force: true },
-        })
+        });
+
+        sequilize.addModels([ProductModel]);
+        await sequilize.sync();
     });
 
     afterEach(async () => {
         await sequilize.close();
     });
-
-    
 });
