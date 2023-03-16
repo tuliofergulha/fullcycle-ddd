822f41527094ed15b6ffbf3edea75bc202576b25
Túlio Fergulha <Tulio.Fergulha@AB-Inbev.com>
2023-03-15 22:47:37 -0300

    Implemented event domain for product

---
+++ src/domain/event/@shared/event-dispatcher.ts
@@ -0,0 +1,25 @@
+import EventDispatcherInterace from "./event-dispatcher.interface";
+import EventHandlerInterface from "./event-handler.interface";
+import EventInterface from "./event.interface";
+
+export default class EventDispatcher implements EventDispatcherInterace {
+
+    private eventHandlers: { [eventName: string]: EventHandlerInterface[] } = {};
+
+    get getEventHandlers(): { [eventName: string]: EventHandlerInterface[] } {
+        return this.eventHandlers;
+    }
+
+    notify(event: EventInterface): void { }
+
+    register(eventName: string, eventHandler: EventHandlerInterface): void { 
+        if (!this.eventHandlers[eventName]) {
+            this.eventHandlers[eventName] = [];
+        }
+        this.eventHandlers[eventName].push(eventHandler);
+    }
+
+    unregister(eventName: string, eventHandler: EventHandlerInterface): void { }
+    
+    unregisterAll(): void { }
+}
